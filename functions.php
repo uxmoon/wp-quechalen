be634f46c6b45a4402d1fa533c5bf1bbe02bc4ca
Martin Luna <martin.luna@rga.com>
2016-12-20 11:45:37 -0300

    slick slider added to functions and app.css

--- functions.php
+++ functions.php
@@ -108,7 +108,9 @@ function quechalen_scripts() {
 
 	wp_enqueue_script( 'quechalen-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'quechalen-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+  wp_enqueue_script( 'quechalen-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'quechalen-slick', get_template_directory_uri() . '/js/slick.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
