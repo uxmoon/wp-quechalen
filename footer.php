bb2d1a62973895c628fc99e03b2b6e04a4601aa3
Martin Luna <martin.luna@rga.com>
2016-12-20 15:41:09 -0300

    added icons from icomoon, updated footer

--- footer.php
+++ footer.php
@@ -14,12 +14,12 @@
   </div><!-- #content -->
 
   <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="site-info-contact">
-      Los Mandarinos S/Nº. Carpintería. San Luis  Teléfono comercial (02656) 49-0117 / Particular (02656) 47.7288  <a href="mailto:info@quechalen.com.ar">info@quechalen.com.ar</a>
-    </div>
+    <ul class="site-info-contact">
+      <li>Los Mandarinos S/Nº. Carpintería. San Luis</li><li>Teléfono comercial (02656) 49-0117 / Particular (02656) 47.7288</li><li><a href="mailto:info@quechalen.com.ar">info@quechalen.com.ar</a></li>
+    </ul>
     <div class="site-info">
-      <p>&copy; <?php echo date(Y); ?> <?php bloginfo( 'name' ); ?>. Todos los derechos reservados.</p>
-      <a href="https://www.facebook.com/cabanas.quechalen">facebook.com/cabanas.quechalen</a>
+      <span>&copy; <?php echo date(Y); ?> <?php bloginfo( 'name' ); ?>. Todos los derechos reservados.</span>
+      <a href="https://www.facebook.com/cabanas.quechalen"><svg class="icon icon-facebook"><use xlink:href="#icon-facebook"></use></svg> facebook.com/cabanas.quechalen</a>
     </div><!-- .site-info -->
   </footer><!-- #colophon -->
 </div><!-- #page -->
