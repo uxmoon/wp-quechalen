5a60dac6d8d33071be80ad19871635123a2cc76f
Martin Luna <martin.luna@rga.com>
2016-12-27 18:06:42 -0300

    added global radius size variable, updated location css and promotions custom loop

--- page-templates/promotions.php
+++ page-templates/promotions.php
@@ -8,18 +8,51 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+      <?php
+      while ( have_posts() ) : the_post();
+      ?>
+
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
+
+        <div class="entry-content">
+          <?php // the_content(); ?>
+
+
+          <?php
+
+    $args = array (
+      'posts_per_page' => -1,
+      'post_type'      => q_promotions,
+    );
+
+    query_posts( $args );
+
+    if ( have_posts() ):
+      while ( have_posts() ) :
+        the_post();
+        the_content();
+      endwhile;
+    else:
+        // Insert any content or load a template for no posts found.
+    endif;
+
+    wp_reset_query();
+
+    ?>
+
+        </div><!-- .entry-content -->
+
+      </article><!-- #post-## -->
+
+
 			<?php
-			while ( have_posts() ) : the_post();
+      endwhile; // End of the loop.
+      ?>
 
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
