c9126d9cad4aa2d071bc156e68bc49e3e4fbb3de
Martin Luna <martin.luna@rga.com>
2016-12-28 15:51:13 -0300

    added cabins css comp file

--- page-templates/cabins.php
+++ page-templates/cabins.php
@@ -8,18 +8,28 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+      <?php
+      while ( have_posts() ) : the_post();
+      ?>
 
-				get_template_part( 'template-parts/content', 'page' );
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+        <div class="entry-content">
+          <div class="c-cabins">
+            <div class="c-cabins__content">
+              <?php the_content(); ?>
+            </div>
+            <div class="c-cabins__gallery">
 
-			endwhile; // End of the loop.
-			?>
+            </div>
+          </div>
+        </div><!-- .entry-content -->
+      </article><!-- #post-## -->
+
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
