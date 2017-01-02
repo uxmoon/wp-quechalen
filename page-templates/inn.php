8505a778481d5d6112491336a63be0406a71e465
Martin Luna <martin.luna@rga.com>
2017-01-02 11:41:50 -0300

    added carousel bxslider markup

--- page-templates/inn.php
+++ page-templates/inn.php
@@ -21,26 +21,29 @@ get_header(); ?>
               <?php the_content(); ?>
             </div>
             <div class="c-cabins__gallery">
-          <?php
-            $args = array (
-              'posts_per_page' => 1,
-              'post_type'      => q_inn,
-            );
+              <?php
+                $args = array (
+                  'posts_per_page' => 1,
+                  'post_type'      => 'q_inn',
+                );
 
-            query_posts( $args );
+                query_posts( $args );
 
-          if ( have_posts() ):
-            while ( have_posts() ) :
-              the_post();
-                $images = rwmb_meta( 'quechalen_imgadv', 'type=image&size=thumbnail-gallery' );
-                foreach ( $images as $image ) {
-                  echo "\n<img src='{$image['url']}' alt='{$image['alt']}' />";
-                }
-            endwhile;
-          else:
-          // Insert any content or load a template for no posts found.
-          endif;
-          wp_reset_query(); ?>
+                if ( have_posts() ):
+                  while ( have_posts() ) :
+                    the_post();
+                    echo "<div class=\"c-carousel\">";
+                      $images = rwmb_meta( 'quechalen_imgadv', 'type=image&size=thumbnail-gallery' );
+                      foreach ( $images as $image ) {
+                        echo "\n<img src='{$image['url']}' alt='{$image['alt']}' />";
+                      }
+                  endwhile;
+                    echo "\n</div>";
+                else:
+                // Insert any content or load a template for no posts found.
+                endif;
+                wp_reset_query();
+              ?>
             </div>
           </div>
         </div><!-- .entry-content -->
