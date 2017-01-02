71ddd8477cdc62ca2bb54c863bc09f91ce5caf13
Martin Luna <martin.luna@rga.com>
2017-01-02 15:36:17 -0300

    removed old modal markup, css and js

--- js/scripts.js
+++ js/scripts.js
@@ -47,21 +47,10 @@
         $('.icon-spinner').css({ visibility: 'hidden' });
     });
 
+    lightbox.option({
+      albumLabel: "Foto %1 de %2",
+    });
+
   });
-
-  // var modal = document.querySelector("#modal");
-  // var modalOverlay = document.querySelector("#modal-overlay");
-  // var closeButton = document.querySelector("#close-button");
-  // var openButton = document.querySelector("#open-button");
-
-  // closeButton.addEventListener("click", function() {
-  //   modal.classList.toggle("closed");
-  //   modalOverlay.classList.toggle("closed");
-  // });
-
-  // openButton.addEventListener("click", function() {
-  //   modal.classList.toggle("closed");
-  //   modalOverlay.classList.toggle("closed");
-  // });
 
 } )( jQuery );
