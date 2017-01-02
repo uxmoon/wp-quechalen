6d03fb2fcfa7cf1c7fc2719d4dfe30b9f851d7a1
Martin Luna <martin.luna@rga.com>
2017-01-02 16:44:28 -0300

    added jquery 2

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -63,3 +63,14 @@ function quechalen_thumbnail_sizes() {
 }
 
 add_action( 'init', 'quechalen_thumbnail_sizes' );
+
+//Making jQuery Google API
+function modify_jquery() {
+  if (!is_admin()) {
+    // comment out the next two lines to load the local copy of jQuery
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', false, '2.2.4');
+    wp_enqueue_script('jquery');
+  }
+}
+add_action('init', 'modify_jquery');
