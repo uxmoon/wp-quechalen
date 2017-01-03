5c449cc3af2e60b8c2d29a9f199c6bbc28253111
Martin Luna <martin.luna@rga.com>
2017-01-03 12:40:24 -0300

    removed modal markup, css and js

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,6 @@
 
   $(document).ready(function(){
 
-    // var modal = document.querySelector("#modal");
-    // var modalOverlay = document.querySelector("#modal-overlay");
-    // var closeButton = document.querySelector("#close-button");
-    // var openButton = document.querySelector("#open-button");
-
-    // closeButton.addEventListener("click", function() {
-    //   modal.classList.toggle("closed");
-    //   modalOverlay.classList.toggle("closed");
-    // });
-
-    // openButton.addEventListener("click", function() {
-    //   modal.classList.toggle("closed");
-    //   modalOverlay.classList.toggle("closed");
-    // });
-
-    $('#open-button').click(function(){
-      $('#modal, #modal-overlay').toggleClass('closed');
-    });
-
-    $('#close-button').click(function(){
-      $('#modal, #modal-overlay').toggleClass('closed');
-    });
-
     var navigationContainer = $('#site-navigation'),
       mainNavigation = navigationContainer.find('.c-anchor-nav ul');
 
@@ -52,6 +29,8 @@
 
       $(this).addClass('current');
       $("#"+tab_id).addClass('current');
+
+      // Required to fix slick slider inside tabs
       $("#"+tab_id+" .c-carousel").slick('setPosition');
     })
 
@@ -70,6 +49,7 @@
         $('.icon-spinner').css({ visibility: 'hidden' });
     });
 
+    // q_gallery
     lightbox.option({
       albumLabel: "Foto %1 de %2",
     });
